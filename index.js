a360c2fc63168907ab4dbce7bfb19c6ee6691e5b
Quoc Ninh <quoctninh@gmail.com>
2022-03-22 19:32:32 -0500

    commit tree 9a4241f1170f9628ce74d3040790a64a1dd97fdb

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (num1 = 2000, num2 = 100) => {
+    return num1 / num2
+}
+
+const square = (x = 2, a = 2) => {
+    return x * 2 
+}
+const add = (a = 3, b = 4) => {
+    return a + b
+}
